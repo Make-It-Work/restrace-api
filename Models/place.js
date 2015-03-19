8a23f954f5f3bc6df6ad668d8ec469549a37e36a
Suzanne <schees@student.avans.nl>
2015-03-19 14:49:58 +0100

    Place schema

--- Models/place.js
+++ Models/place.js
@@ -2,7 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var placeSchema = new Schema({
-
+	location            : {
+        lat  : Boolean,
+        lng  : Boolean,
+    },
+    name : String
 });
 
 mongoose.model('place', placeSchema);
