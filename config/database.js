c3fc268930c0b26b127a12e2ad4e4a6c508f8a14
Make-It-Work <swhjenni@student.avans.nl>
2015-03-24 15:34:14 +0100

    DB change link on host

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
 // config/database.js
+var url;
+console.log(process.env.SESSIONNAME);
+if(process.env.SESSIONNAME === 'Console') {
+	url = 'mongodb://localhost:27017/restrace';
+	console.log('localhost');
+} else {
+	console.log('else');
+	url = process.env.databaseconnection;
+}
 module.exports = {
-
-	'url' : process.env.databaseconnection;
-
+	'url' : url
 };
