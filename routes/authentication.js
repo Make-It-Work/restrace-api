d74203191c379987d496b473a613aab923b03b21
Make-It-Work <swhjenni@student.avans.nl>
2015-03-25 14:36:40 +0100

    Working json login

--- routes/authentication.js
+++ routes/authentication.js
@@ -9,9 +9,16 @@ module.exports = function(app, passport) {
 
 	// PROFILE SECTION =========================
 	app.get('/profile', function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user
-		});
+		var user = {
+			"user": req.user
+		};
+		if (req.query.returnType === 'json') {
+			res.json(user);	
+		} else {
+			res.render('profile.ejs', {
+				user : req.user
+			});
+		}
 	});
 
 	// LOGOUT ==============================
@@ -32,11 +39,20 @@ module.exports = function(app, passport) {
 		});
 
 		// process the login form
-		app.post('/login', passport.authenticate('local-login', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/login', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+		app.post('/login', 
+			passport.authenticate('local-login'), 
+			function(req, res) {
+				var user = req.user
+				if (req.query.returnType === 'json') {
+					res.json(req.user);	
+				} else {
+					console.log(user);
+					res.render('profile.ejs', {
+						"user": user
+					});
+				}
+			}
+		);
 
 		// SIGNUP =================================
 		// show the signup form
