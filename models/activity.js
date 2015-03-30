c8ba930bcac3981457f5bda63cceac2233821150
schees <schees@student.avans.nl>
2015-03-30 15:50:44 +0200

    Place id in google id veranderd

--- models/activity.js
+++ models/activity.js
@@ -4,14 +4,14 @@ var	Place = mongoose.model('Place');
 
 //--------------------------------Schema----------------------------------------------------
 var activitySchema = new Schema({
-	place_id : {type : Schema.Types.ObjectId, ref:"place", required:true},
+    google_id : {type : Number, ref:"place", required:true},
 	description : {type : String, required : true}
 });
 
 //--------------------------------Validation----------------------------------------------------
 	// validate place_id is existing place
-activitySchema.path('place_id').validate(function (value, respond){
-	Place.findOne({_id: value}, function (err, doc) {
+activitySchema.path('google_id').validate(function (value, respond){
+	Place.findOne({google_id: value}, function (err, doc) {
         if (err || !doc) {
             respond(false);
         } else {
